e92c99718daecc2e182cd0124f97dc10bfc02375
Samuel Njoroge <108589210+Samuel-Njoroge@users.noreply.github.com>
2022-09-13 20:51:19 +0300

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -6,7 +6,7 @@ import pyperclip
 
 
 
-###initialize window
+#initialize window
 
 root =Tk()
 root.geometry("400x400")
@@ -19,14 +19,14 @@ Label(root, text ='DataFlair', font ='arial 15 bold').pack(side = BOTTOM)
 
 
 
-###select password length
+#select password length
 pass_label = Label(root, text = 'PASSWORD LENGTH', font = 'arial 10 bold').pack()
 pass_len = IntVar()
 length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 15).pack()
 
 
 
-#####define function
+#define function
 
 pass_str = StringVar()
 
@@ -40,13 +40,13 @@ def Generator():
    
 
 
-###button
+#button
 
 Button(root, text = "GENERATE PASSWORD" , command = Generator ).pack(pady= 5)
 
 Entry(root , textvariable = pass_str).pack()
 
-########function to copy
+#function to copy
 
 def Copy_password():
     pyperclip.copy(pass_str.get())
